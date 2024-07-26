e870c7430cccf4092c9a81c928854ab5613ab18f
Taiwo-AB <63714619+Taiwo-AB@users.noreply.github.com>
2024-07-26 15:59:44 +0100

    DDCE-5686 Bump coverage to 100 + dependency upgrades as part of KT (#22)
    DDCE-5686 Bump coverage to 100 + dependency upgrades as part of KT
    
    DDCE-5686 Bump coverage to 100 + dependency upgrades as part of KT

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -1,20 +1,12 @@
 import sbt.Setting
-import scoverage.ScoverageKeys
+import scoverage.ScoverageKeys.*
 
 object CodeCoverageSettings {
 
-  private val excludedPackages: Seq[String] = Seq(
-    "<empty>",
-    "Reverse.*",
-    "app.*",
-    "prod.*"
-  )
-
   val settings: Seq[Setting[?]] = Seq(
-    ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 99,
-    ScoverageKeys.coverageFailOnMinimum := true,
-    ScoverageKeys.coverageHighlighting := true
+    coverageExcludedFiles := ".*Routes.*",
+    coverageMinimumStmtTotal := 100,
+    coverageFailOnMinimum := true,
+    coverageHighlighting := true
   )
-
 }
