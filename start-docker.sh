b16f8d33263ed1be7d9b76d24f7d4b2fd9eb7d83
suraj-suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2025-03-19 12:00:31 +0000

    DDCE-5824: certificate logic

--- start-docker.sh
+++ start-docker.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-CLIENT_JKS_FILE=$(find . -type f -name client.jks)
-
-# Overwrite the keystore with a client certificate.
-if [ -n "${CLIENT_JKS}" ] ; then
-  if [ -f $CLIENT_JKS_FILE ]; then
-    echo "$CLIENT_JKS" | base64 --decode > "$CLIENT_JKS_FILE"
-  else
-    echo "Cannot find client.jks file location."
-  fi
-fi
+#CLIENT_JKS_FILE=$(find . -type f -name client.jks)
+#
+## Overwrite the keystore with a client certificate.
+#if [ -n "${CLIENT_JKS}" ] ; then
+#  if [ -f $CLIENT_JKS_FILE ]; then
+#    echo "$CLIENT_JKS" | base64 --decode > "$CLIENT_JKS_FILE"
+#  else
+#    echo "Cannot find client.jks file location."
+#  fi
+#fi
 
 SCRIPT=$(find . -type f -name home-office-immigration-status-proxy)
 exec $SCRIPT $HMRC_CONFIG -Dconfig.file=conf/home-office-immigration-status-proxy.conf
