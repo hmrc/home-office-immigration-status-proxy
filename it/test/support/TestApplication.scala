fb092b22ac25462608764e4dab58aaec125ae02c
grantrobson <grant.robson@digital.hmrc.gov.uk>
2025-11-13 08:18:09 +0000

    172 WIP

--- it/test/support/TestApplication.scala
+++ it/test/support/TestApplication.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,27 +14,43 @@
  * limitations under the License.
  */
 
-package support
-
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
-
-trait TestApplication { baseSpec: BaseISpec =>
-
-  override implicit lazy val app: Application = appBuilder.build()
-
-  protected override def appBuilder: GuiceApplicationBuilder =
-    new GuiceApplicationBuilder()
-      .configure(
-        "microservice.services.home-office-right-to-public-funds.port"       -> wireMockServer.port(),
-      //  "microservice.services.home-office-right-to-public-funds.host"       -> wireMockServer.port(),
-//        "microservice.services.home-office-right-to-public-funds.pathPrefix" -> "/v1",
-        "microservice.services.auth.port"                                    -> wireMockServer.port(),
-     //   "microservice.services.auth.host"                                    -> wireMockHost,
-        "metrics.enabled"                                                    -> false,
-        "auditing.enabled"                                                   -> false,
-//        "auditing.consumer.baseUri.host"                                     -> wireMockHost,
-        "auditing.consumer.baseUri.port"                                     -> wireMockServer.port()
-      )
-
-}
+///*
+// * Copyright 2024 HM Revenue & Customs
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package support
+//
+//import play.api.Application
+//import play.api.inject.guice.GuiceApplicationBuilder
+//
+//trait TestApplication { baseSpec: BaseISpec =>
+//
+//  override implicit lazy val app: Application = appBuilder.build()
+//
+//  protected override def appBuilder: GuiceApplicationBuilder =
+//    new GuiceApplicationBuilder()
+//      .configure(
+//        "microservice.services.home-office-right-to-public-funds.port"       -> wireMockServer.port(),
+//      //  "microservice.services.home-office-right-to-public-funds.host"       -> wireMockServer.port(),
+////        "microservice.services.home-office-right-to-public-funds.pathPrefix" -> "/v1",
+//        "microservice.services.auth.port"                                    -> wireMockServer.port(),
+//     //   "microservice.services.auth.host"                                    -> wireMockHost,
+//        "metrics.enabled"                                                    -> false,
+//        "auditing.enabled"                                                   -> false,
+////        "auditing.consumer.baseUri.host"                                     -> wireMockHost,
+//        "auditing.consumer.baseUri.port"                                     -> wireMockServer.port()
+//      )
+//
+//}
